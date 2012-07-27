bc98ad8d9dec7f3db1e374392110ed2fd5195a3a
moserluk <moser.lukca@gmail.com>
2012-07-27 18:13:44 +0200

    detail projektu a similar projects

--- library/Utils.php
+++ library/Utils.php
@@ -204,4 +204,28 @@ function currentServerURL() {
  return $pageURL;
 }
 
+/*
+ * Trim the text
+ *
+ */
+function shorter($text, $chars_limit)
+{
+    // Check if length is larger than the character limit
+    if (strlen($text) > $chars_limit)
+    {
+        // If so, cut the string at the character limit
+        $new_text = substr($text, 0, $chars_limit);
+        // Trim off white space
+        $new_text = trim($new_text);
+        // Add at end of text ...
+        return $new_text . "...";
+    }
+    // If not just return the text as is
+    else
+    {
+    return $text;
+    }
+}
+
+
 ?>
